0b3b096d128903f733e0cd71c023bf4353aada6a
Bram van Oosterhout <bram@van-oosterhout.org>
2020-02-01 22:23:16 +1100

    my first test scenario

--- foswiki/webs/assets/Solution1.sh
+++ foswiki/webs/assets/Solution1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -LJO https://github.com/BramVan-Oosterhout/katacoda-scenarios/blob/master/foswiki/webs/assets/Solution1.tar
+curl -LJo Solution1.tgz https://github.com/BramVan-Oosterhout/katacoda-scenarios/blob/master/foswiki/webs/assets/Solution1.tgz
 cd /var/www/foswiki/data/Sandbox 
-tar -xvf /root/Solution1.tar
+tar -xvzf /root/Solution1.tgz
 chmod www-data:www-data Solution1
