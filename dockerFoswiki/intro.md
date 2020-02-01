752b7f4f141b8d429d67a5a649556badcb41f999
Bram van Oosterhout <bram@van-oosterhout.org>
2020-02-01 22:35:15 +1100

    my first test scenario

--- dockerFoswiki/intro.md
+++ dockerFoswiki/intro.md
@@ -82,3 +82,4 @@ Last update: Saturday 1 February  22:04:21 AEDT 2020
 Last update: Saturday 1 February  22:10:33 AEDT 2020
 Last update: Saturday 1 February  22:23:16 AEDT 2020
 Last update: Saturday 1 February  22:29:22 AEDT 2020
+Last update: Saturday 1 February  22:35:15 AEDT 2020
--- foswiki/webs/assets/Solution1.sh
+++ foswiki/webs/assets/Solution1.sh
@@ -2,4 +2,4 @@
 curl -LJo Solution1.tgz https://github.com/BramVan-Oosterhout/katacoda-scenarios/blob/master/foswiki/webs/assets/Solution1.tgz?raw=true
 cd /var/www/foswiki/data/Sandbox 
 tar -xvzf /root/Solution1.tgz
-chmod www-data:www-data Solution1
+chown www-data:www-data Solution1
