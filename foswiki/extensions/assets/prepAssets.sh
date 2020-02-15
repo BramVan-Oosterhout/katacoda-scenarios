4cf5f1508728a7a57484206ea0e207af6237a4f6
Bram van Oosterhout <bram@van-oosterhout.org>
2020-02-15 15:49:56 +1100

    my first test scenario

--- foswiki/extensions/assets/prepAssets.sh
+++ foswiki/extensions/assets/prepAssets.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Prepare the assets directory
-cp ~/foswiki/Foswiki-2.1.2/data/Sandbox/SolutionsExtenstiondStep2.txt ~/programs/Katacoda/katacoda-scenarios/foswiki/extensions/assets/.
+cp ~/foswiki/Foswiki-2.1.2/data/Sandbox/SolutionExtensionsStep2.txt ~/programs/Katacoda/katacoda-scenarios/foswiki/extensions/assets/.
--- foswiki/extensions/step2-answer.md
+++ foswiki/extensions/step2-answer.md
@@ -1,4 +1,4 @@
-Run the following command to install the solution: `step1a-answer.sh`{{execute}}
+Run the following command to install the solution: `cp SolutionExtensionsStep2.txt .var/www/foswiki/data/Sandbox/.`{{execute}}
 and inspect solution page:
 
 *   _Sandbox.SolutionExtentensionsStep1_
