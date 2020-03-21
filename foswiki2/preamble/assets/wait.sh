34fe356ad7e741c4ff3482507e553292649c1cb9
Bram van Oosterhout <bram@van-oosterhout.org>
2020-03-21 17:29:19 +1100

    Update: Saturday 21 March  17:29:19 AEDT 2020

--- foswiki2/preamble/assets/wait.sh
+++ foswiki2/preamble/assets/wait.sh
@@ -7,6 +7,8 @@ echo -n "+"
 sleep 2
 done
 } 2>/dev/null
+echo ""
+export DEFAULT_URL_HOST=http://[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {Password}='password'"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {Sessions}{UseIPMatching}='0'"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {PermittedRedirectHostUrls}='$DEFAULT_URL_HOST'"
--- foswiki2/preamble/setup.sh
+++ foswiki2/preamble/setup.sh
@@ -3,5 +3,4 @@ if [[ $(docker inspect foswiki 2>/dev/nul) == '[]' ]]; then
 docker run -d --name foswiki -p 80:80 timlegge/docker-foswiki
 fi
 until [ "`/usr/bin/docker inspect -f {{.State.Running}} foswiki`"=="true" ]; do sleep 1; done
-export DEFAULT_URL_HOST=http://[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com
 touch Foswiki_config_complete
