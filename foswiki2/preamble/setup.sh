58b35342143f913c7d35c2575a8472457d9de16e
Bram van Oosterhout <bram@van-oosterhout.org>
2020-03-21 16:08:24 +1100

    Update: Saturday 21 March  16:08:24 AEDT 2020

--- foswiki2/preamble/setup.sh
+++ foswiki2/preamble/setup.sh
@@ -2,7 +2,9 @@
 if [[ $(docker inspect foswiki 2>/dev/nul) == '[]' ]]; then
 docker run -d --name foswiki -p 80:80 timlegge/docker-foswiki
 fi
-sleep 10
+while [ $(docker inspect foswiki 2>/dev/nul) == '[]' ]; do
+sleep 1
+done
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {Password}='password'"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {Sessions}{UseIPMatching}='0'"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {PermittedRedirectHostUrls}='http://[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com/'"
