eb016caf3b3927567c19fece213894431b8efdf0
Bram van Oosterhout <bram@van-oosterhout.org>
2020-03-24 18:41:05 +1100

    Update: Tuesday 24 March  18:41:05 AEDT 2020

--- foswiki2/preamble/assets/wait.sh
+++ foswiki2/preamble/assets/wait.sh
@@ -13,7 +13,7 @@ docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save 
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {Sessions}{UseIPMatching}='0'"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {PermittedRedirectHostUrls}='$1'"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {Plugins}{CaptchaPlugin}{Enabled}='0'"
-docker exec -it foswiki//bin/bash -c "sed -i '/server_name/a      add_header \"Access-Control-Allow-Origin\"  *;' /etc/nginx/conf.d/default.conf"
-docker exec -it foswiki//bin/bash -c "/usr/sbin/nginx -s reload"
+docker exec -it foswiki /bin/bash -c "sed -i '/server_name/a      add_header \"Access-Control-Allow-Origin\"  *;' /etc/nginx/conf.d/default.conf"
+docker exec -it foswiki /bin/bash -c "/usr/sbin/nginx -s reload"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash
 echo " done."
