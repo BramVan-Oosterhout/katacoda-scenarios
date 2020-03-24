b3ecea5852f5367549f28f3c83023b2abfe100c9
Bram van Oosterhout <bram@van-oosterhout.org>
2020-03-24 13:21:48 +1100

    Update: Tuesday 24 March  13:21:48 AEDT 2020

--- foswiki2/preamble/assets/wait.sh
+++ foswiki2/preamble/assets/wait.sh
@@ -12,6 +12,5 @@ docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "touch data/.htpasswd"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {Password}='password'"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {Sessions}{UseIPMatching}='0'"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {PermittedRedirectHostUrls}='$1'"
-docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {Plugins}{CaptchaPlugin}{Enabled}='0'"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash
 echo " done."
