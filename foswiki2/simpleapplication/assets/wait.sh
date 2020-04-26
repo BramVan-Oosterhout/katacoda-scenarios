3038118cbaccd7f1273996bd476957692016c975
Bram van Oosterhout <bram@van-oosterhout.org>
2020-04-26 14:16:43 +1000

    Update: Sun 26 Apr 14:16:43 AEST 2020

--- foswiki2/simpleapplication/assets/wait.sh
+++ foswiki2/simpleapplication/assets/wait.sh
@@ -9,7 +9,7 @@ done
 } 2>/dev/null
 echo " >>"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "touch data/.htpasswd"
-docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {Password}='password'"
+docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {Password}='password' >/dev/null"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {Sessions}{UseIPMatching}='0'"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {DefaultUrlHost}='https://$1'"
 docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "tools/configure -save -set {ForceDefaultUrlHost}='1'"
@@ -21,5 +21,5 @@ docker exec -it -w /var/www/foswiki foswiki /bin/bash -c "sed -i 's/Set SKIN/#Se
 docker exec -it -w /var/www/foswiki/bin foswiki /bin/bash -c "./view /Main/Webhome -refresh=all >/dev/null"
 docker exec -it foswiki /bin/bash -c "sed -i '/server_name/a      add_header \"Access-Control-Allow-Origin\"  *;' /etc/nginx/conf.d/default.conf"
 docker exec -it foswiki /bin/bash -c "/usr/sbin/nginx -s reload"
-docker exec -it -w /var/www/foswiki foswiki /bin/bash
 echo " done."
+docker exec -it -w /var/www/foswiki foswiki /bin/bash
