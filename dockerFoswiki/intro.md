d0092f5153f1e61b5862db6b4cd4e76f9c39418c
Bram van Oosterhout <bram@van-oosterhout.org>
2020-07-24 14:51:51 +1000

    Update: Fri 24 Jul 14:51:50 AEST 2020

--- dockerFoswiki/intro.md
+++ dockerFoswiki/intro.md
@@ -92,3 +92,4 @@ Last update: Wed 22 Jul 16:39:38 AEST 2020
 Last update: Wed 22 Jul 16:50:13 AEST 2020
 Last update: Fri 24 Jul 13:35:02 AEST 2020
 Last update: Fri 24 Jul 13:53:05 AEST 2020
+Last update: Fri 24 Jul 14:51:50 AEST 2020
--- foswiki2/notepadapp/assets/prepAssets.sh
+++ foswiki2/notepadapp/assets/prepAssets.sh
@@ -2,9 +2,9 @@
 # Prepare the assets directory
 mydir=`pwd`
 pushd ~/devfoswiki/core/data/Applications
-tar cvzf $mydir/NotePadApp.tgz  NotePadApp/*
-tar cvzf $mydir/SolNotePadApp.tgz --exclude Web* SolNotePadApp/*
+tar --owner nginx --group nginx -cvzf $mydir/NotePadApp.tgz  NotePadApp/*
+tar --owner nginx --group nginx -cvzf $mydir/SolNotePadApp.tgz --exclude Web* SolNotePadApp/*
 popd
 pushd ~/devfoswiki/core/data/Sandbox
-tar cvzf $mydir/SolNotePad.tgz SolNotePad/*
+tar --owner nginx --group nginx -cvzf $mydir/SolNotePad.tgz SolNotePad/*
 popd
