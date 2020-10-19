0d3a0835d5440ad1fa7dfaff903ef285e9fc7591
Bram van Oosterhout <bram@van-oosterhout.org>
2020-10-19 17:05:19 +1100

    Update: Mon 19 Oct 17:05:19 AEDT 2020

--- foswiki0/testFoswiki/setup.sh
+++ foswiki0/testFoswiki/setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-git clone https://github.com/BramVan-Oosterhout/katacoda-foswiki-images
- 
+if [[ $(docker inspect foswiki 2>/dev/nul) == '[]' ]]; then
+docker run -d --name foswiki -p 80:80 timlegge/docker-foswiki
+fi
+until [ "`/usr/bin/docker inspect -f {{.State.Running}} foswiki`"=="true" ]; do sleep 1; done
+touch Foswiki_config_complete
