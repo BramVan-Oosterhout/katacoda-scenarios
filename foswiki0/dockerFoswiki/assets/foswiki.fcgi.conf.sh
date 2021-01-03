df3b039082f3669d725edb1c598ffdb3a4af4198
Bram van Oosterhout <bram@van-oosterhout.org>
2021-01-03 14:33:38 +1100

    updated from TutorialPlugin by www-data  on 03/00/2021 14:33

--- foswiki0/dockerFoswiki/assets/foswiki.fcgi.conf.sh
+++ foswiki0/dockerFoswiki/assets/foswiki.fcgi.conf.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# Conventional init scripts (Should also work with systemd)
+echo "...starting apache+foswiki"
+cd /var/www/foswiki/bin
 
-cp /var/www/foswiki/tools/foswiki.init-script /etc/init.d/foswiki
-# make the file executable using
-chmod +x /etc/init.d/foswiki
-# and ensure that it is assigned to user/group root
+./foswiki.fcgi -l 127.0.0.1:9000 -n 5 -d --pidfile /tmp/foswiki.pid
 
-cp /var/www/foswiki/tools/foswiki.defaults /etc/default/foswiki
-# and make appropriate adjustments:
-# Make sure the process uses the same socket as configured in nginx (see above, defaults to 127.0.0.1:9000)
-# verify that the FOSWIKI_ROOT setting points to your foswiki installation. 
-
-#start foswiki
-/etc/init.d/foswiki start
+nginx -g "daemon off;"
