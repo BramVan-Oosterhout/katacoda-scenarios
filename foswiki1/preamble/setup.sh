6be8b56a77031c7644b9dfc687bbb20bdf4c6029
BramVan-Oosterhout <bram@van-oosterhout.org>
2021-05-31 11:03:22 +1000

    add gaurd to setup.sh

--- foswiki1/preamble/setup.sh
+++ foswiki1/preamble/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+until [ -e KcBvO.crt && -e KcBvO.key && -e Foswikicgi.dck && -e foswiki.cgi.conf ] 
+do
+   echo -n '+'
+done
 docker build -t foswiki -f Foswikicgi.dck .
 docker run -d --rm --name foswiki -p 443:443 foswiki
