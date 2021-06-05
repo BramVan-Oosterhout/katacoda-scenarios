09bb4a5ab8fb02d84e6dee44238843b7e6d6c796
BramVan-Oosterhout <bram@van-oosterhout.org>
2021-06-05 22:43:53 +1000

    Updated from TutorialPlugin by BramVanOosterhout(www-data)

--- foswiki1/preamble/setup.sh
+++ foswiki1/preamble/setup.sh
@@ -7,5 +7,5 @@ do
    sleep 3
 done
 docker build -t foswiki -f Foswikicgi.dck .
-docker run -d --rm --name foswiki -p 443:443 foswiki
+docker run -d --rm --name foswiki -p 80:80 foswiki
 touch foswiki-running
