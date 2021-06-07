3c4cf9cf0bd50b014a5c8ec2e2d9a489f15a5ea1
BramVan-Oosterhout <bram@van-oosterhout.org>
2021-06-07 17:10:05 +1000

    update setup

--- foswiki1/webs/setup.sh
+++ foswiki1/webs/setup.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-until [[ -e KcBvO.crt \
-      && -e KcBvO.key \
-      && -e Foswikicgi.dck 
-      && -e foswiki.cgi.conf ]] 
+docker pull bramvanoosterhout/foswiki1
+docker run -d --rm --name foswiki -p 80:80 bramvanoosterhout/foswiki1
+until [[ -e answer.pl ]] 
 do
    sleep 3
 done
-docker pull bramvanoosterhout/foswiki1
-docker run -d --rm --name foswiki -p 80:80 bramvanoosterhout/foswiki1
-# > > >docker cp answer foswiki:/tmp/.
-# > > >docker exec -it foswiki /bin/bash -c "apk add tar"
-#docker build -t foswiki -f Foswikicgi.dck .
-#docker run -d --rm --name foswiki -p 80:80 foswiki
+docker cp answer.pl foswiki:/tmp/answer
+docker exec -it -w /var/www/foswiki foswiki /bin/bash
 touch foswiki-running
