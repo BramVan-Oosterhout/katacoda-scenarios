8fb6498990577d9b3eda3be25a955fb2f307e297
BramVan-Oosterhout <bram@van-oosterhout.org>
2021-06-10 22:41:16 +1000

    revise setup.sh. run the built image

--- foswiki1/webs/setup.sh
+++ foswiki1/webs/setup.sh
@@ -4,5 +4,5 @@ do
    sleep 1
 done
 docker build -t foswiki -f webs.dck .
-docker run -d --rm --name foswiki -p 80:80 bramvanoosterhout/foswiki1
+docker run -d --rm --name foswiki -p 80:80 foswiki
 touch foswiki-running
