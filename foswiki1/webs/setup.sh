2c951c440244551804435815e5b00a1d9a77db33
BramVan-Oosterhout <bram@van-oosterhout.org>
2021-06-10 16:02:16 +1000

    added and use webs.dck

--- foswiki1/webs/setup.sh
+++ foswiki1/webs/setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-docker pull bramvanoosterhout/foswiki1
-docker run -d --rm --name foswiki -p 80:80 bramvanoosterhout/foswiki1
-until [[ -e answer.pl ]] 
+until [[ -e answer.pl && -e webs,dck ]] 
 do
-   sleep 3
+   sleep 1
 done
-until [ "`/usr/bin/docker inspect -f {{.State.Running}} foswiki`"=="true" ]; do sleep 1; done
-docker cp answer.pl foswiki:/tmp/answer
-docker exec -it -w /var/www/foswiki foswiki /bin/bash
+docker build -t foswiki -f webs.dck .
+docker run -d --rm --name foswiki -p 80:80 bramvanoosterhout/foswiki1
 touch foswiki-running
