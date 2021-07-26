f1acb0dc1a54040fbc63c0fe8802ca7247f4a23d
BramVan-Oosterhout <bram@van-oosterhout.org>
2021-07-26 16:30:23 +1000

    updated exercise

--- first-course/example/assets/answer.pl
+++ first-course/example/assets/answer.pl
@@ -2,5 +2,5 @@
 my %steps = ( step1 => \&step1 );
 &{$steps{$ARGV[0]}};
 sub step1 {
-  print "Hello from answer.pl - step1 running on hostname\n";
+  print "Hello from answer.pl - step1 running on `hostname`\n";
 }
