43fe156d50cb65664d19d8d164b354f45f50a458
BramVan-Oosterhout <bram@van-oosterhout.org>
2021-07-29 22:27:53 +1000

    updated exercise

--- first-course/example/setup.sh
+++ first-course/example/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-until [[ -e example-course.dck ]]
+until [[ -e first-course.dck ]]
 do
    sleep 1
 done
-docker build -t foswiki -f example-course.dck .
+docker build -t foswiki -f first-course.dck .
 docker run -d --rm --name foswiki -p 80:80 foswiki
 touch foswiki-running
